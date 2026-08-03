df6128479b9dbb1cb756323d1f16c771ab390b3d
Jason Geller <drjasongeller@gmail.com>
2026-08-03 14:58:31 -0400

    update nix files for cogmod and fix depend and remote issues with it

--- create_env_dev.R
+++ create_env_dev.R
@@ -2,12 +2,12 @@
 # Sys.setenv(PATH = paste("/nix/var/nix/profiles/default/bin", Sys.getenv("PATH"), sep=":"))
 
 required_packages <- c(
+  "tidyverse",
+  "fs",
   "brms",
-  "easystats",
   "ggplot2",
   "ggdist",
   "tinytable",
-  "ggdist",
   "patchwork",
   "knitr",
   "rmarkdown",
@@ -15,7 +15,8 @@ required_packages <- c(
   "reformulas",
   "collapse",
   "ragg",
-  "qs2"
+  "qs2",
+  "emmeans"
 )
 
 library(rix)
@@ -38,6 +39,36 @@ rix(
       package_name = "cmdstanr",
       repo_url = "https://github.com/stan-dev/cmdstanr",
       commit = "541f36c74c236a322eaa0908e2e86425790ca2cf"
+    ),
+    list(
+      package_name = "insight",
+      repo_url = "https://github.com/easystats/insight",
+      commit = "36917c3bedfadf901ea8b0a90c5e144ae82bcfb8"
+    ),
+    list(
+      package_name = "datawizard",
+      repo_url = "https://github.com/easystats/datawizard",
+      commit = "b651172a20c86aca0d768b44a634c83fb146cafd"
+    ),
+    list(
+      package_name = "bayestestR",
+      repo_url = "https://github.com/easystats/bayestestR",
+      commit = "77d649a2f55481e0b863c0d35b2a14dba7f44afb"
+    ),
+    list(
+      package_name = "parameters",
+      repo_url = "https://github.com/easystats/parameters",
+      commit = "9fb4f21f2452cfb1c8e6511e0cfa521db5d7d7de"
+    ),
+    list(
+      package_name = "performance",
+      repo_url = "https://github.com/easystats/performance",
+      commit = "78c41eec5285896e8ae86776979ff7b4f41f38ae"
+    ),
+    list(
+      package_name = "modelbased",
+      repo_url = "https://github.com/easystats/modelbased",
+      commit = "e12e4a96c396b754a6b29c1df3215c1c0057e287"
     ),
     list(
       package_name = "cogmod",
